98c6a34d10e5d39843ff246bb9827c3df78158c1
Unknown <zwepab16@htlkaindorf.at>
2019-05-27 11:55:26 +0200

    Java Doc added to BL

--- Tipp-Trainer/src/PlayBl.java
+++ Tipp-Trainer/src/PlayBl.java
@@ -9,9 +9,16 @@ import javax.swing.AbstractListModel;
 
 public class PlayBl extends AbstractListModel {
 
+    /**
+     * The list and the current text varaible are here!
+     */
     private ArrayList list = new ArrayList<File>();
     private String nowText;
 
+    /**
+     * Searches for all .txt files in the "levels" folder
+     * and saves them into the list
+     */
     public PlayBl() {
 
         File f = new File(".\\levels");
@@ -23,16 +30,30 @@ public class PlayBl extends AbstractListModel {
         }
     }
 
+    /**
+     * 
+     * @return Size of List 
+     */
     @Override
     public int getSize() {
         return list.size();
     }
 
+    /**
+     * 
+     * @param i The Selected Level
+     * @return element at the right position! 
+     */
     @Override
     public String getElementAt(int i) {
         return list.get(i).toString();
     }
-
+/**
+ * 
+ * @param lvl The User's selected Level
+ * @return The text from the selected Level from a .txt file!
+ * @throws IOException 
+ */
     public String enterGame(int lvl) throws IOException {
 
         BufferedReader reader = new BufferedReader(new FileReader((File)list.get(lvl)));
@@ -49,6 +70,9 @@ public class PlayBl extends AbstractListModel {
 
     }
 
+    /**
+     * @return the Text the User is playing!
+     */
     public String getNowText() {
         return nowText;
     }
